a35b2bdea83c1137095d99c6621af56b4170a162
cuong tran <cuong@silentium.io>
2025-11-07 12:23:47 +0700

    Add shadowing project - learning variable shadowing in Rust

---
+++ shadowing/main.rs
@@ -0,0 +1,10 @@
+fn main() {
+    let x = 5;
+    let x = x + 1;
+    println!("The value of x before in scope is: {}", x);
+    {
+        let x = x * 2;
+        println!("The value of x in the inner scope is: {}", x);
+    }
+    println!("The value of x after in scope is: {}", x);
+}
